cf3d63e610bdaba49001e509bdf48fdd8c99a334
rdosreis <rodrigocpdosreis@gmail.com>
2021-08-10 18:49:37 -0300

    aula 02

---
+++ RScripts/01_delin_pesquisa.R
@@ -0,0 +1,27 @@
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.height='80%', out.width='55%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'dor-de-cabeca-1.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.height='80%', out.width='55%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'dor-de-cabeca-2.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'estudos-observacionais.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='right', message=FALSE, warning=FALSE, out.width='25%', paged.print=FALSE----
+knitr::include_graphics(here('images','pesquisa_convid.jpg'))
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'Statistically-Insignificant-ff.jpg'))
+
+
