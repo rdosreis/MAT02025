5e529603b5626713a9b5617f25b65d69290ce118
rdosreis <rodrigocpdosreis@gmail.com>
2021-09-02 06:51:01 -0300

    aula 09

--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "07_revisao_prob_e_cb"
+arquivo <- "09_aas_prop_estimadores"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
