06e4cb3356a9694754ed41ed6124606e22ea941e
rdosreis <rodrigocpdosreis@gmail.com>
2021-09-23 00:20:44 -0300

    aula 14 - Estimativa de um índice

--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "13_aas_com_reposicao"
+arquivo <- "14_aas_estimativa_indice"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
