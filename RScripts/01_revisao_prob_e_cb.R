cb81df997bd58d0b2cc6fa1e11ac19d8ae45c6da
rdosreis <rodrigocpdosreis@gmail.com>
2022-06-20 18:22:14 -0300

    2022/1 - Aulas 01 e 02: revisão prob e estat

---
+++ RScripts/01_revisao_prob_e_cb.R
@@ -0,0 +1,155 @@
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'prob-01.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'prob-02.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'prob-02b.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'prob-03.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'prob-04.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'va-01.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'va-02.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'va-03.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'va-04.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'va-05.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'va-06.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'dist-prob-01.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'dist-prob-02.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'dist-prob-03.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'dist-prob-04.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'dist-prob-05.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'dist-prob-06.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'dist-prob-07.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'esperanca_variancia-01.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'esperanca_variancia-02.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'esperanca_variancia-03.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'esperanca_variancia-04.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'esperanca_variancia-05.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'esperanca_variancia-06.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='80%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'esperanca_variancia-07.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'Statistically-Insignificant-corda-bamba.jpg'))
+
+
---
+++ RScripts/02_revisao_prob_e_cb.R
@@ -0,0 +1,131 @@
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-pop.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-amostra.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-01.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-02.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-03.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-04.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-05.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-06.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-07.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-08.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-09.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-10.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-11.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-12.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-13.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-14.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-15.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-16.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-17.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-18.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'inf-estat-19.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'Statistically-Insignificant-corda-bamba.jpg'))
+
+
