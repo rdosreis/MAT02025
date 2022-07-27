9c32c4002600902ad67443313cc4e568e65950da
rdosreis <rodrigocpdosreis@gmail.com>
2022-07-27 12:46:27 -0300

    Aula 10 + Aula 09 corrigida

--- RScripts/09_aas_prop_estimadores.R
+++ RScripts/09_aas_prop_estimadores.R
@@ -1,16 +1,16 @@
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE-------------------
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
 
 knitr::include_graphics(here::here('images', 'aula-revisao', 'aas-01.png'))
 
 
 
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE-------------------
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
 
 knitr::include_graphics(here::here('images', 'aula-revisao', 'aas-02.png'))
 
 
 
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE-------------------
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
 
 knitr::include_graphics(here::here('images', 'aula-revisao', 'aas-03.png'))
 
--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "09_aas_prop_estimadores"
+arquivo <- "10_aas_var_estimadores"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
