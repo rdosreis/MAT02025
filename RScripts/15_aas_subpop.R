a0fce1b11a33696b8c6df95130f64cab2d3614fe
rdosreis <rodrigocpdosreis@gmail.com>
2022-08-16 00:42:20 -0300

    aula 15

--- RScripts/15_aas_subpop.R
+++ RScripts/15_aas_subpop.R
@@ -1,34 +1,5 @@
-## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------------------------
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='90%', paged.print=FALSE----
 
-library(survey)
-data(api)
-
-# View(apisrs)
-# ?apisrs
-
-# O objeto design
-api.des <- svydesign(id = ~1,
-                     fpc = ~fpc,
-                     data = apisrs)
-
-# Estimativa do total de alunos
-# matriculados
-svytotal(x = ~enroll, design = api.des)
-
-# Estimativa da média de alunos
-# matriculados por escola
-svymean(x = ~enroll, design = api.des)
-
-
-
-## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------------------------
-
-# Estimativa do total de alunos
-# matriculados por tipo de escola
-svyby(formula = ~enroll, by = ~stype, design = api.des, FUN = svytotal)
-
-# Estimativa da média de alunos
-# matriculados por tipo de escola
-svyby(formula = ~enroll, by = ~stype, design = api.des, FUN = svymean)
+knitr::include_graphics(here::here('images', 'aula-revisao', 'dm-02.png'))
 
 
--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "14_aas_estimativa_indice"
+arquivo <- "15_aas_subpop"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
