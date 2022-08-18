5594bdf2f105c813b4a9d0029409f34a625f35cd
rdosreis <rodrigocpdosreis@gmail.com>
2022-08-17 23:42:47 -0300

    Aula 16

--- RScripts/16_aas_tcl_popfin.R
+++ RScripts/16_aas_tcl_popfin.R
@@ -1,24 +1,32 @@
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='60%'--------------
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='70%'------------
 
-y <- trees$Volume
+library(ggplot2)
+
+data("diamonds")
+
+# y <- trees$Volume
+
+y <- diamonds$carat
 N <- length(y)
 hist(y, probability = T,
      main = "",
      xlab = "Y",
      ylab = "Densidade",
      border = "white",
-     col = "#1B9E77")
+     col = "#1B9E77",
+     breaks = 30)
 
 
 
 
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%'-------------
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%'-----------
+
+b <- 10000
+
+n <- 1
 
 ybar <- numeric(0)
 
-b <- 10000
-n <- 1
-
 for (k in 1:b){s <- sample(1:N, n); ybar[k] <- mean(y[s])}
 
 hist(ybar, probability = T,
@@ -26,32 +34,21 @@ hist(ybar, probability = T,
      xlab = expression(bar(y)),
      ylab = "Densidade",
      border = "white",
-     col = "#D95F02",
-     xlim = c(10,80))
+     col = "#D95F02", breaks = 30)
 
 legend("topright", legend = "n = 1", bty = "n")
 
-
-
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%'-------------
-
-n <- 2
-
-for (k in 1:b){s <- sample(1:N, n); ybar[k] <- mean(y[s])}
-
-hist(ybar, probability = T,
-     main = "",
-     xlab = expression(bar(y)),
-     ylab = "Densidade",
-     border = "white",
-     col = "#D95F02",
-     xlim = c(10,80))
-
-legend("topright", legend = "n = 2", bty = "n")
+enes <- c(rep(1, b),
+          rep(5, b),
+          rep(20, b),
+          rep(50, b),
+          rep(100, b),
+          rep(1000, b))
+ybares <- ybar
 
 
 
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%'-------------
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%'-----------
 
 n <- 5
 
@@ -62,34 +59,17 @@ hist(ybar, probability = T,
      xlab = expression(bar(y)),
      ylab = "Densidade",
      border = "white",
-     col = "#D95F02",
-     xlim = c(10,80))
+     col = "#D95F02", breaks = 30)
 
 legend("topright", legend = "n = 5", bty = "n")
 
-
-
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%'-------------
-
-n <- 15
-
-for (k in 1:b){s <- sample(1:N, n); ybar[k] <- mean(y[s])}
-
-hist(ybar, probability = T,
-     main = "",
-     xlab = expression(bar(y)),
-     ylab = "Densidade",
-     border = "white",
-     col = "#D95F02",
-     xlim = c(10,80))
-
-legend("topright", legend = "n = 15", bty = "n")
+ybares <- c(ybares, ybar)
 
 
 
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%'-------------
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%'-----------
 
-n <- 25
+n <- 20
 
 for (k in 1:b){s <- sample(1:N, n); ybar[k] <- mean(y[s])}
 
@@ -98,16 +78,17 @@ hist(ybar, probability = T,
      xlab = expression(bar(y)),
      ylab = "Densidade",
      border = "white",
-     col = "#D95F02",
-     xlim = c(10,80))
+     col = "#D95F02", breaks = 30)
 
-legend("topright", legend = "n = 25", bty = "n")
+legend("topright", legend = "n = 20", bty = "n")
+
+ybares <- c(ybares, ybar)
 
 
 
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%'-------------
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%'-----------
 
-n <- 30
+n <- 50
 
 for (k in 1:b){s <- sample(1:N, n); ybar[k] <- mean(y[s])}
 
@@ -116,10 +97,69 @@ hist(ybar, probability = T,
      xlab = expression(bar(y)),
      ylab = "Densidade",
      border = "white",
-     col = "#D95F02",
-     xlim = c(10,80))
+     col = "#D95F02", breaks = 30)
 
-legend("topright", legend = "n = 30", bty = "n")
+legend("topright", legend = "n = 50", bty = "n")
+
+ybares <- c(ybares, ybar)
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%'-----------
+
+n <- 100
+
+for (k in 1:b){s <- sample(1:N, n); ybar[k] <- mean(y[s])}
+
+hist(ybar, probability = T,
+     main = "",
+     xlab = expression(bar(y)),
+     ylab = "Densidade",
+     border = "white",
+     col = "#D95F02", breaks = 30)
+
+legend("topright", legend = "n = 100", bty = "n")
+
+ybares <- c(ybares, ybar)
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%'-----------
+
+n <- 1000
+
+for (k in 1:b){s <- sample(1:N, n); ybar[k] <- mean(y[s])}
+
+hist(ybar, probability = T,
+     main = "",
+     xlab = expression(bar(y)),
+     ylab = "Densidade",
+     border = "white",
+     col = "#D95F02", breaks = 30)
+
+legend("topright", legend = "n = 1000", bty = "n")
+
+ybares <- c(ybares, ybar)
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%'-----------
+
+df.joy <- data.frame(enes, ybares)
+df.joy$y.bar.center <- (df.joy$ybares - mean(y))/sqrt(var(y)*(1 - df.joy$enes/N)*(1/df.joy$enes))
+df.joy$enes.char <- as.factor(df.joy$enes)
+
+library(ggjoy)
+p <- ggplot(data = df.joy,
+            mapping = aes(x = y.bar.center, y = enes.char, fill = enes.char)) + 
+  geom_joy(alpha = 0.7) + 
+  scale_x_continuous(breaks = c(-3, -2, -1, 0, 1, 2, 3)) +
+  scale_fill_brewer(palette = "Greys") +
+  theme_joy() +
+  labs(x = "z", y = "n", caption = "Processo centralizado.") +
+  theme(legend.position = "none")
+p
+
 
 
 
