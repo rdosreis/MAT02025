200e7f9ff3a7e004dfcf76fca0494a12cab69ea9
rdosreis <rodrigocpdosreis@gmail.com>
2022-09-22 08:18:41 -0300

    Aula 21 - Tam de amostra!

--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "21_aas_prop_subpop"
+arquivo <- "22_ta_intro"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
