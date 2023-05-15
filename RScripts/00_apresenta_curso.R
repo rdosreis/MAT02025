149bddf44735bf1ead14f514b943cc94fefe580c
rdosreis <rodrigocpdosreis@gmail.com>
2023-05-15 18:29:33 -0300

    Começo 2023

--- RScripts/00_apresenta_curso.R
+++ RScripts/00_apresenta_curso.R
@@ -1,22 +1,14 @@
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='80%', out.height='80%', paged.print=FALSE----
-
-knitr::include_graphics(here::here('images', 'hi_my_name_is.png'))
-
-
-
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
-knitr::include_graphics(here::here('images','covid-recomendacoes.jpg'))
-
-
 ## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='10%', paged.print=FALSE----
+
 knitr::include_graphics(here::here('images','Rlogo.png'))
 
 
-## ---- echo=FALSE, eval=TRUE-------------------------------------------------------------------
+
+## ---- echo=FALSE, eval=TRUE---------------------------------------------------------------------
 x <- rnorm(n = 100, mean = 10, sd = 1)
 
 
-## ---- echo=TRUE, eval=TRUE, fig.align='center', out.width='50%'-------------------------------
+## ---- echo=TRUE, eval=TRUE, fig.align='center', out.width='50%'---------------------------------
 hist(x, col = 'black', border = 'white')
 
 
@@ -51,11 +43,5 @@ knitr::include_graphics(here::here('images', 'pesquisa-opiniao.jpg'))
 ## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', out.height='80%', paged.print=FALSE----
 
 knitr::include_graphics(here::here('images', 'ibge-pesquisas.jpg'))
-
-
-
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='50%', paged.print=FALSE----
-
-knitr::include_graphics(here::here('images', 'Statistically-Insignificant-8.jpg'))
 
 
--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "23_ta_formulas"
+arquivo <- "00_apresenta_curso"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
