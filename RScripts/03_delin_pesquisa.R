23b0c9ed7d2f2b4df059ec4700fd7786909757f7
rdosreis <rodrigocpdosreis@gmail.com>
2023-05-28 19:48:57 -0300

    Aula 03

--- RScripts/03_delin_pesquisa.R
+++ RScripts/03_delin_pesquisa.R
@@ -4,6 +4,24 @@ knitr::include_graphics(here::here('images', 'dor-de-cabeca-3.jpg'))
 
 
 
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='75%', paged.print=FALSE, fig.cap="Uso de tabaco e mortalidade por câncer de pulmão, EUA, 1900-1997. Asterisco (*) Por 100.000 e ajustado por idade para a população padrão dos EUA em 1970."----
+
+knitr::include_graphics(here::here('images', 'consumo_tabaco_cancer.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='85%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'caso-controle.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'coorte.png'))
+
+
+
 ## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
 
 knitr::include_graphics(here::here('images', 'estudos-observacionais.jpg'))
--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "02_revisao_prob_e_cb"
+arquivo <- "03_delin_pesquisa"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
