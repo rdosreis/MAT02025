88073264512f37f8116021787b01157f28dc91bf
rdosreis <rodrigocpdosreis@gmail.com>
2023-07-09 22:34:48 -0300

    Aula 10

--- RScripts/10_aas_var_estimadores.R
+++ RScripts/10_aas_var_estimadores.R
@@ -1,9 +1,3 @@
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
-
-knitr::include_graphics(here::here('images', 'aula-revisao', 'aas-04.png'))
-
-
-
 ## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='50%', paged.print=FALSE----
 
 knitr::include_graphics(here::here('images', 'Statistically-Insignificant-capediem.jpg'))
