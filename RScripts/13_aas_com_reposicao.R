79ac6a754bb4cc29d77de94d2507e01a48feee60
rdosreis <rodrigocpdosreis@gmail.com>
2023-07-18 22:51:49 -0300

    Aula 13

--- RScripts/13_aas_com_reposicao.R
+++ RScripts/13_aas_com_reposicao.R
@@ -1,16 +1,4 @@
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
-
-knitr::include_graphics(here::here('images', 'aula-revisao', 'aas-01.png'))
-
-
-
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
-
-knitr::include_graphics(here::here('images', 'aula-revisao', 'aas-05.png'))
-
-
-
-## ---- echo=TRUE, eval=TRUE, message=FALSE, warning=FALSE--------------------------------------
+## ---- echo=TRUE, eval=TRUE, message=FALSE, warning=FALSE--------------------------------------------
 
 sample(x = 1:528, size = 10, replace = TRUE)
 
