4468916373fc8f9e72aa92db61b2fa495beba23d
rdosreis <rodrigocpdosreis@gmail.com>
2023-07-25 22:35:12 -0300

    Aulas 14 e 15

---
+++ RScripts/14_15_aas_estimativa_indice_+_subpop.R
@@ -0,0 +1,11 @@
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'dm-01.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='90%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'aula-revisao', 'dm-02.png'))
+
+
--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "14_aas_estimativa_indice"
+arquivo <- "14_15_aas_estimativa_indice_+_subpop"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
